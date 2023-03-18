2e5f624cd6ff708066d2230749920e2d38929de6
Somebody54569 <sorawis.leep@bumail.net>
2023-03-18 10:37:25 +0700

    finish Basic char

--- Assets/Scripts/Staff.cs
+++ Assets/Scripts/Staff.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Staff : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _id;
+    public int ID { get { return _id;} set { _id = value; } }
+
+    private int charSkinId;
+    public int CharSkinId { get { return charSkinId;} set { charSkinId = value; } }
+    public GameObject[] charSkin;
+
+    public string staffName;
+    public int dailyWage;
+
+    public void InitCharID(int id)
     {
-        
+        _id = id;
+        charSkinId = Random.Range(0, charSkin.Length - 1);
+        staffName = "XXXX";
+        dailyWage = Random.Range(80, 125);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeCharSkin()
     {
-        
+        for (int i = 0; i < charSkin.Length; i++)
+        {
+            if (i == charSkinId)
+            {
+                charSkin[i].SetActive(true);
+            }
+            else
+            {
+                charSkin[i].SetActive(false);
+            }
+        }
     }
 }
